95bfb8a3b8fac93481b6734102c5441fdb9aefff
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-06 13:02:58 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const PATHS = {
+    source: path.join(__dirname, 'src'),
+    build: path.join(__dirname, 'public'),
+};
+
+module.exports = {
+    entry: `${PATHS.source}/index.js`,
+
+    output: {
+        path: PATHS.build,
+        filename: 'js/bundle.js',
+        publicPath: '/'
+    },
+
+    mode: NODE_ENV === 'development' ? 'development' : 'production',
+
+    devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : false,
+
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        }),
+        new webpack.NamedModulesPlugin()
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: PATHS.source,
+                loader: 'babel-loader',
+                sideEffects: false
+            }
+        ]
+    },
+
+    devServer: {
+        stats: 'errors-only',
+        host: '0.0.0.0',
+        port: 3000,
+        contentBase: PATHS.build,
+        hot: true,
+        historyApiFallback: true
+    },
+};
+
+if (NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                sourceMap: false,
+                beautify: false,
+                comments: false,
+                mangle: {
+                    keep_fnames: true
+                },
+                compress: {
+                    sequences: true,
+                    booleans: true,
+                    loops: true,
+                    unused: true,
+                    warnings: false,
+                    drop_console: true
+                }
+            }
+        })
+    );
+}
