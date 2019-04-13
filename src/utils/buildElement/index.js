fec89a2152657ee62dd85f1835ce162f2eecaf61
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-13 23:58:15 +0300

    Update "buildElement" function

--- src/utils/buildElement/index.js
+++ src/utils/buildElement/index.js
@@ -4,12 +4,13 @@ import 'core-js/modules/es6.array.find';
  * Builds form element
  * @param {HTMLElement} node
  * @param {Array} elementRules
+ * @param {Object} config
  * @returns {Object}
  */
-const buildElement = (node, elementRules) => {
+const buildElement = (node, elementRules = [], config = {}) => {
     const {Element} = elementRules.find(({check}) => check(node));
 
-    return new Element(node);
+    return new Element(node, config);
 };
 
 export default buildElement;
