c436a872ed234ab49d9773c7e8c41a3aa12d6adb
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-13 23:57:45 +0300

    Update config. Add default visibility state handlers.

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,7 @@
 export const attributeName = 'data-rules';
+
 export const formEvents = ['change', 'blur', 'focus', 'keydown', 'keypress', 'keyup'];
+
+export const onShow = (root) => root.style.display = 'block'; // eslint-disable-line
+
+export const onHide = (root) => root.style.display = 'none'; // eslint-disable-line
