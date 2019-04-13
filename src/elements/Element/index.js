036157dbd4715b1c982b87508eefa38fbe8560d3
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-13 23:54:09 +0300

    Update "Element" element

--- src/elements/Element/index.js
+++ src/elements/Element/index.js
@@ -1,12 +1,28 @@
+import onShow from './modules/onShow';
+import onHide from './modules/onHide';
+import onReset from './modules/onReset';
+import onUpdate from './modules/onUpdate';
+import parseRules from '../../utils/parseRules';
+
+/**
+ * Creates a new Element class
+ */
 class Element {
-    constructor(root, rules = []) {
+    constructor(root, config = {}) {
         this.root = root;
-        this.rules = rules;
+        this.name = root.name;
+        this.config = config;
+        this.rules = parseRules(root.getAttribute(this.config.attributeName));
     }
 
     get value() {
         return this.root.value;
     }
 }
+
+Element.prototype.onShow = onShow;
+Element.prototype.onHide = onHide;
+Element.prototype.onReset = onReset;
+Element.prototype.onUpdate = onUpdate;
 
 export default Element;
